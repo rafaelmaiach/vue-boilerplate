0d5a3e9badfad16bb6c59250880c0b533654d5f4
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-05-22 22:06:16 -0300

    chore: improve jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
+	preset: '@vue/cli-plugin-unit-jest',
+	testMatch: ['**/*.spec.js'],
+	transformIgnorePatterns: ['<rootDir>/node_modules'],
+	collectCoverageFrom: [
+		'src/**/*.{js,vue}',
+		'!src/main.js',
+		'!src/registerServiceWorker.js',
+		'!src/store/**/index.js',
+		'!src/store/**/mutationsTypes.js',
+		'!src/router/index.js',
+		'!src/i18n/index.js',
+	],
 };
