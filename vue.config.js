a5a2e7426852cd125ff9ed30bdbc1b9e2a1532b9
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-05-22 22:30:09 -0300

    chore: vue config
    - Configure devServer proxy
    - Improve bundle size by getting a small version of VueTypes

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
+const path = require('path');
+
+const { NODE_ENV, VUE_APP_CORE_URL } = process.env;
+
+const isProduction = NODE_ENV === 'production';
+const backendUrl = VUE_APP_CORE_URL || 'http://localhost:3000';
+
 module.exports = {
-  lintOnSave: false,
+	lintOnSave: false,
+
+	devServer: {
+		proxy: {
+			'^/api': {
+				target: backendUrl,
+			},
+		},
+	},
+
+	chainWebpack: (config) => {
+		if (isProduction) {
+			const vueTypesShimPath = path.resolve(__dirname, 'node_modules/vue-types/es/shim.js');
+			config.resolve.alias.set('vue-types', vueTypesShimPath);
+		}
+	},
 };
