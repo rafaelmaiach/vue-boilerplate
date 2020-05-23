5a8e97e0cd810eca276c46c9b60ed7adc94a8663
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-05-22 23:21:36 -0300

    refactor: store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+
+import modules from './modules';
+import persistanceConfig from './persistence.config';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {
-	},
-	mutations: {
-	},
-	actions: {
-	},
-	modules: {
-	},
+	state: {},
+	mutations: {},
+	actions: {},
+	modules,
+	plugins: [
+		createPersistedState(persistanceConfig),
+	],
 });
